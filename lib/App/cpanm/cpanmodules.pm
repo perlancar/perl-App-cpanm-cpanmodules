ebaa39382e2117c4dcd5d6f97658562e92ab197b
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2021-05-22 08:12:42 +0700

    Release v0.001

---
+++ lib/App/cpanm/cpanmodules.pm
@@ -0,0 +1,14 @@
+package App::cpanm::cpanmodules;
+
+# DATE
+# VERSION
+
+1;
+
+# ABSTRACT: Install all modules listed in an Acme::CPANModules::* module
+
+=head1 DESCRIPTION
+
+See included script L<cpanm-cpanmodules>.
+
+=cut
